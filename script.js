9babda0cf1b9a14723948a9241960dbc89c93df7
unknown <vbud@SF055.softservecom.com>
2015-06-11 11:43:43 +0300

    Added script. Inside - function to print result of pushing butoons on the screen. Chaged index.html to call function to print on the screen at pushing button.

---
+++ script.js
@@ -0,0 +1,10 @@
+﻿function printOnScreen(entry) {
+	if(document.getElementById("screen").innerText==0){
+	document.getElementById("screen").innerText = entry.toString();
+	}else{
+		document.getElementById("screen").innerText=
+		document.getElementById("screen").innerText +
+		entry.toString();
+	}
+}
+
